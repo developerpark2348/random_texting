32a6ca5e55c17d0bdb6b5608aaa7ac89c80c1947
developerpark2348 <developerpark2348@gmail.com>
2021-03-02 16:45:37 +0900

    base css and html
    기본 베이스 css, html을 제작함.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navbar } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <Navbar className="header">
+        <div className="header_text">랜텍</div>
+      </Navbar>
+      <Navbar className="Nav">
+          <img
+            className="Nav_img"
+            src="../mail.png"
+            alt="메일 이미지"
+          />
+          <div className="Nav_title">
+            랜텍 - 당신이 작성하는 랜덤 텍스트
+          </div>
+      </Navbar>
+      <div className="Main">
+        <div className="balloon">
+          <div className="balloon_text">
+            파이어베이스에서 꺼내오는 랜덤 텍스트1
+          </div>
+        </div>
+        <div className="balloon">
+          <div className="balloon_text">
+            파이어베이스에서 꺼내오는 랜덤 텍스트2
+          </div>
+        </div>
+        <div className="balloon">
+          <div className="balloon_text">
+            파이어베이스에서 꺼내오는 랜덤 텍스트3
+          </div>
+        </div>
+      </div>
+      <div className="Input">
+        <input type="text" className="Input_input"placeholder="랜덤 텍스트를 작성해주세요!"/>
+        <button className="Input_Btn" placeholder="입력하기">입력</button>
+      </div>
     </div>
   );
 }
