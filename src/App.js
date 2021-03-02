49f151885b783910906791687cf44744cee4b681
developerpark2348 <developerpark2348@gmail.com>
2021-03-02 18:20:28 +0900

    Add Main
    Main을 프로젝트에 추가함.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
+import React from 'react';
 import './App.css';
 import { Navbar } from 'react-bootstrap';
 
 function App() {
+
   return (
     <div className="body">
       <Navbar className="header">
-        <div className="header_text">랜텍</div>
+        <div className="header_text">추천1등 ~ 추천2등 ~</div>
       </Navbar>
       <Navbar className="Nav">
           <img
             className="Nav_img"
             src="../mail.png"
             alt="메일 이미지"
-          />
+          /> 
           <div className="Nav_title">
             랜텍 - 당신이 작성하는 랜덤 텍스트
           </div>
       </Navbar>
       <div className="Main">
-        <div className="balloon">
-          <div className="balloon_text">
-            파이어베이스에서 꺼내오는 랜덤 텍스트1
+        <div className="Main_balloon">
+          <div className="balloon">
+            <div className="balloon_text">
+              파이어베이스에서 꺼내오는 랜덤 텍스트1
+            </div>
           </div>
-        </div>
-        <div className="balloon">
-          <div className="balloon_text">
-            파이어베이스에서 꺼내오는 랜덤 텍스트2
-          </div>
-        </div>
-        <div className="balloon">
-          <div className="balloon_text">
-            파이어베이스에서 꺼내오는 랜덤 텍스트3
+          <div className="Main_thumb">
+                <button>추천</button>
+                <div className="Main_thumb_num">추천 수 : 0</div>
           </div>
         </div>
       </div>
       <div className="Input">
         <input type="text" className="Input_input"placeholder="랜덤 텍스트를 작성해주세요!"/>
-        <button className="Input_Btn" placeholder="입력하기">입력</button>
+        <button className="Input_Btn" placeholder="입력하기" />
       </div>
     </div>
   );
