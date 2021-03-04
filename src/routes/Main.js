b446e6f58b1e11768d74b05f243c792bb46100ee
developerpark2348 <developerpark2348@gmail.com>
2021-03-04 16:34:51 +0900

    enter키나 버튼을 누를시 해당 입력된 텍스트가 db에 들어가는것을 구현함.

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,21 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Main.css';
 import { Navbar } from 'react-bootstrap';
 import { authService } from '../fbase';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt, faShare } from '@fortawesome/free-solid-svg-icons';
+import { dbService } from '../fbase';
 
 function Main() {
+
+    const [text, setText] = useState("");
+    const [texts, setTexts] = useState([]);
 
     const LogOut = () => {
         authService.signOut();
     }
-    
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await dbService.collection("text").add({
+            text: text,
+            createdAt: Date.now(),
+            thumbs: 0
+        });
+        alert("저장되었습니다!")
+        setText("");
+    };
+
+    const onChange = (event) => {
+        const {target:{value},} = event;
+        setText(value);
+    }
+
+    const getTexts = async() => {
+        const dbtexts = await dbService.collection("text").get()
+        dbtexts.forEach((document) => {
+            const dbtextsObject = {
+                ...document.data(),
+                id: document.id,
+            }
+            setTexts(prev => [dbtextsObject, ...prev]);
+        })
+    }
+
+    useEffect(()=>{
+        getTexts();
+    }, [])
+    console.log(texts);
 
 return (
     <div className="body">
         <Navbar className="header">
             <div className="header_text">추천1등 ~ 추천2등 ~</div>
-            <button className="header_LogoutBtn" onClick={LogOut}>로그아웃</button>
-            <button className="header_KakaoBtn" onClick={LogOut}>공유하기</button>
+            <button className="header_LogoutBtn" onClick={LogOut}>
+                <FontAwesomeIcon icon={faSignOutAlt}/>
+            </button>
+            <button className="header_KakaoBtn">
+                <FontAwesomeIcon icon={faShare}/>
+            </button>
         </Navbar>
         <Navbar className="Nav">
             <img
@@ -31,7 +72,11 @@ return (
             <div className="Main_balloon">
                 <div className="balloon">
                     <div className="balloon_text">
-                        파이어베이스에서 꺼내오는 랜덤 텍스트1
+                        {texts.map((texts) => (
+                            <div key={texts.id}>
+                                <div>{texts.text}</div>
+                            </div>
+                        ))}
                     </div>
                 </div>
                 <div className="Main_thumb">
@@ -42,10 +87,21 @@ return (
                 </div>
             </div>
         </div>
-        <div className="Input">
-            <input type="text" className="Input_input"placeholder="랜덤 텍스트를 작성해주세요!"/>
-            <button className="Input_Btn" placeholder="입력하기" />
-        </div>
+        <form className="Input" onSubmit={onSubmit}>
+            <input 
+                value={text} 
+                onChange={onChange} 
+                type="text" 
+                className="Input_input" 
+                placeholder="랜덤 텍스트를 작성해주세요!" 
+                maxLength={40}
+            />
+            <input 
+                type="submit" 
+                className="Input_Btn" 
+                value=""
+            />
+        </form>
     </div>
     );
 }
